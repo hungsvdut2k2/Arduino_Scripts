7b701af22c91e1f9ebcbda987e47e612318023bb
cyantiz <nhan053qt@gmail.com>
2022-12-08 21:00:25 +0700

    fix: dc motor

--- dc_motor/dc_motor.ino
+++ dc_motor/dc_motor.ino
@@ -1,114 +1,74 @@
-// Ket not Ong ca A 
-int enA = 9;
-int in1 = 8;
-int in2 = 7; 
+int IN1 = 9;
+int IN2 = 10;
 
-// Ket not Ong co B 
-int enB = 3; 
-int in3 = 5; 
-int in4 = 4; 
-int value1 =0; 
-int value2 =0; 
-int value3 =0; 
-int value4 =0; 
-int value5 =0; 
-void setup()  {
-    Serial.begin(9600); 
-    for(int i = 10; i <= 13; i++) { 
-        pinMode(i, INPUT);
-        pinMode(2, INPUT);
-        // Set tat ca cac chan dieu khien drgng co thanh output 
-        pinMode(enA, OUTPUT); 
-        pinMode(enB, OUTPUT); 
-        pinMode(in1, OUTPUT); 
-        pinMode(in2, OUTPUT); 
-        pinMode(in3, OUTPUT); 
-        pinMode(in4, OUTPUT); 
-        // Tat tat ca Ong co - Trang thai ban Tau 
-        digitalWrite(in1, LOW); 
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, LOW);
-    } 
-void loop() { 
-    value1 = digitalRead(13); 
-    //stop 
-    if(valuel = HIGH)
-    { 
-        Serial.println("S1 OK");
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, LOW); 
-        digitalWrite(in3, LOW); 
-        digitalWrite(in4, LOW); 
-    }  
-    //xoay oil/1g chieu kim dOng hO 50 
-    value2 = digitalRead(12); 
-    if(value2 = HIGH) {
-        Serial.println("S2 OK"); 
-        analogWrite(enA, 255);
-        analogWrite(enB, 255);         
-        digitalWrite(in1, HIGH);
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, LOW);
-        //digitalWrite(inl, LOW); 
-        //digitalWrite(in2, HIGH); 
-        //digitalWrite(in3, LOW);  
-        //digitalWrite(in4, HIGH); 64 
-    } 
-    //ngucc chieu kim Ong 11.6 66 
-    value3 = digitalRead(11); 
-    if(value3 == HIGH) {  
-        Serial.println("53 OK");
-        analogWrite(enA, 255);
-        analogWrite(enB, 255);
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH); 
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, HIGH);
+int potpin = 0; // A0
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+}
+
+void loop()
+{
+
+    // doc val tu bien tro
+    int val = analogRead(potpin);
+    Serial.println(val);
+
+    int speed = val / 4;
+
+    // -> run with speed by val
+    motorRun(speed);
+
+    // -> spin with max speed
+    // motorRun(-1);
+
+    // -> spin with speed by val (reverse)
+    // motorRunReverse(speed);
+
+    // -> spin with max speed (reverse)
+    // motorRunReverse(-1);
+
+    // -> stop motor
+    // motorStop();
+
+    delay(100);
+}
+
+void motorStop()
+{
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+}
+
+void motorRun(int speed)
+{
+    if (speed == -1)
+    {
+        digitalWrite(IN1, HIGH);
+        digitalWrite(IN2, LOW);
     }
-    value4 = digitalRead(10);
-    if(value4 = HIGH){ 
-    // Tgt tat ca cdc Ong co 
-        digitalWrite(in1, LOW); 
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, LOW);
-        Serial.println("S4 OK");
-        // Khoi Ong cac Ong co 
-        analogWrite(enA, 0);
-        analogWrite(enB, 0);
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, HIGH);
-        // TAng toc to 0 den tOc dq tOi da 
-        for (int i = 0; i < 256; i++) { 
-             analoqWrite(enA, i); 
-             analogWrite(enB, i); 
-             delay(20);  
-        }  
-    } 
-    value5 = digitalRead(2);
-    if(value5 == HIGH) { 
-        Serial.println("B4 OK");  
-        // TAt tat ca cac dOng co 
-        digitalWrite(in1, LOW); 
-        digitalWrite(in2, LOW); 
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, LOW);
-        // Khai dOng cac Ong co 
-        analogWrite(enA, 255);
-        analogWrite(enB, 255);
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, HIGH);
-        // Giam tOc to tOc do tOi da ve 0 
-        for (int i = 255; i >= 0; i--) { 
-            analogWrite(enA, i); 
-            analogWrite(enB, i); 
-            delay(20); 
-            } 
-        } 
+
+    if (speed != -1)
+    {
+        analogWrite(IN1, speed);
+        digitalWrite(IN2, LOW);
     }
+}
+
+void motorRunReverse(int speed)
+{
+    if (speed == -1)
+    {
+        digitalWrite(IN1, LOW);
+        digitalWrite(IN2, HIGH);
+    }
+
+    if (speed != -1)
+    {
+        analogWrite(IN1, LOW);
+        digitalWrite(IN2, speed);
+    }
+}
